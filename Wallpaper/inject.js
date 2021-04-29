50abaa91af3da722ebfa1c8fc3dd945632d6c35e
plumk <plumk97@outlook.com>
2021-04-29 23:28:42 +0800

    remove inject.js log code

--- Wallpaper/inject.js
+++ Wallpaper/inject.js
@@ -1,5 +1,4 @@
 function wallpaper_mouseMoveEvent(x, y) {
-    console.log(x, y)
     var event = document.createEvent("MouseEvent")
     event.initMouseEvent("mousemove", true, false, window, 1, x, y, x, y, false, false, false, false, 0, null)
     document.dispatchEvent(event)
@@ -7,7 +6,6 @@ function wallpaper_mouseMoveEvent(x, y) {
 
 
 function wallpaper_mouseDownEvent(x, y) {
-    console.log(x, y)
     var event = document.createEvent("MouseEvent")
     event.initMouseEvent("mousedown", true, false, window, 1, x, y, x, y, false, false, false, false, 0, null)
     document.dispatchEvent(event)
@@ -15,7 +13,6 @@ function wallpaper_mouseDownEvent(x, y) {
 
 
 function wallpaper_mouseUpEvent(x, y) {
-    console.log(x, y)
     var event = document.createEvent("MouseEvent")
     event.initMouseEvent("mouseup", true, false, window, 1, x, y, x, y, false, false, false, false, 0, null)
     document.dispatchEvent(event)
